62e262a9fbe61a371f7fbc5ed226460875846ab4
Patryk <patrykowo@gmail.com>
2022-10-10 12:23:24 +0200

    Fix navigation styles.

--- resources/views/includes/navigation.blade.php
+++ resources/views/includes/navigation.blade.php
@@ -1,15 +1,15 @@
-<nav class="navbar navbar-expand-lg p-0 px-1 main--nav">
+<nav class="navbar navbar-expand-lg navbar-dark p-0 px-1 main--nav">
     <div class="container-fluid">
         <a href="{{ route('main.index') }}">
             <img src="{{ asset('storage/assets/logo/norella.png') }}" class="logo" alt="Norella logo">
         </a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+        <button class="navbar-toggler border-0 p-0" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
             <span class="navbar-toggler-icon"></span>
         </button>
 
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <!-- Left Side Of Navbar -->
-            <ul class="navbar-nav me-auto">
+            <ul class="navbar-nav ms-auto me-0">
                 <li class="nav-item {{ Route::is('main.index') ? 'active' : '' }}">
                     <a class="nav-link mx-md-3 py-lg-1" href="{{ route('main.index') }}">Strona Główna</a>
                 </li>
@@ -30,17 +30,17 @@
                 </li>
             </ul>
 
+            @auth
             <!-- Right Side Of Navbar -->
             <ul class="navbar-nav ms-auto">
                 <!-- Authentication Links -->
-                @auth
                 <li class="nav-item dropdown">
-                    <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                    <a id="navbarDropdown" class="nav-link dropdown-toggle text-light" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                         {{ Auth::user()->name }}
                     </a>
 
                     <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                        <a class="dropdown-item" href="{{ route('logout') }}"
+                        <a class="dropdown-item text-light" href="{{ route('logout') }}"
                             onclick="event.preventDefault();
                                             document.getElementById('logout-form').submit();">
                             {{ __('Logout') }}
@@ -50,8 +50,8 @@
                         </form>
                     </div>
                 </li>
-                @endauth
             </ul>
+            @endauth
         </div>
     </div>
 </nav>
