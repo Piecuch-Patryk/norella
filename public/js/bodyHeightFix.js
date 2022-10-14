8acfa56f6082522087762c6553cd747efbb8a51b
Patryk <patrykowo@gmail.com>
2022-10-14 11:23:05 +0200

    Remove unused js files.

--- public/js/bodyHeightFix.js
+++
@@ -1,5 +0,0 @@
-$(document).ready(function(){
-    $('body')
-        .css('min-height', '100vh')
-        .addClass('d-flex flex-wrap align-content-between');
-});
--- public/js/masonry-init.js
+++
@@ -1,9 +0,0 @@
-$('.grid').imagesLoaded( {
-    itemSelector: '.grid-item',
-    columnWidth: '.grid-sizer',
-    fitwidth: true,
-  },
-  function() {
-    $('.grid').masonry();
-  }
-);
